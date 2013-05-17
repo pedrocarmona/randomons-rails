3e8e8e3646a86172df189008cf6743615b5a9f0d
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-05-17 04:35:26 +0100

    Fix for Heroku failure during asset compilation.

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Randomons
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Force the application not to access the DB or load models when precompiling assets.
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
