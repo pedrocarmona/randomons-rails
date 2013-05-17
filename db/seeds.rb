02cdf7d3cfc7cbf262f650211592b5cd503d0bd1
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-05-17 03:21:51 +0100

    Added a few seeds as examples.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+include Type, Status, ExperienceFunction
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Move.create([
+  { name: 'Water gun', attack: 40, move_types: [Type::WATER] },
+  { name: 'Firespit', attack: 60, move_types: [Type::FIRE], status: Status::BURN, status_probability: 10 }
+])
+
