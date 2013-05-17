3dbbf4c44cb17cbeebbdeef71d59d88d7e68a96a
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-05-17 04:09:59 +0100

    Fixed registration bug.

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -5,8 +5,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   def create
-    build_resource(:registration)
-    #resource.skip_confirmation!
+    build_resource
     if resource.save
       sign_in resource
       render :status => 200,
