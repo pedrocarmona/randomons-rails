1693efdfe59b1bef75b7e03374cf3aa56e9fe648
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-05-26 01:59:46 +0100

    Added validations to specie move.

--- app/models/specie_move.rb
+++ app/models/specie_move.rb
@@ -3,4 +3,13 @@ class SpecieMove < ActiveRecord::Base
   belongs_to :move
 
   attr_accessible :specie_id, :move_id, :level
+
+  validates :specie_id, :move_id,
+    :presence => true
+
+  validates :level,
+    :presence => true,
+    :numericality => {
+      :greater_than_or_equal_to => 0
+    }
 end
