ee0074ee2199c6b9b0cca0b85340927305b730b1
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-05-26 22:12:06 +0100

    A user can now have items.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,14 @@ class User < ActiveRecord::Base
          :token_authenticatable
 
   has_many :creatures
+  has_many :user_items, :dependent => :destroy
+  has_many :items, :through => :user_items
 
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :user_items_attributes
+
+  accepts_nested_attributes_for :user_items,
+    :allow_destroy => true,
+    :reject_if => lambda { |a| a[:item_id].blank? }
 
   before_save :ensure_authentication_token
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130526195320) do
+ActiveRecord::Schema.define(:version => 20130526205624) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -120,6 +120,16 @@ ActiveRecord::Schema.define(:version => 20130526195320) do
   end
 
   add_index "species", ["name"], :name => "index_species_on_name", :unique => true
+
+  create_table "user_items", :force => true do |t|
+    t.integer  "user_id",                   :null => false
+    t.integer  "item_id",                   :null => false
+    t.integer  "quantity",   :default => 1, :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "user_items", ["user_id"], :name => "index_user_items_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
