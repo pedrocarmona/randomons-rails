7a8a3172e22b59c851fabf6b82d9850710c905c8
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-05-26 02:28:03 +0100

    Added example user to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,5 @@
 include Type, Status, ExperienceFunction
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Move.create([
-  { name: 'Water gun', attack: 40, move_types: [Type::WATER] },
-  { name: 'Firespit', attack: 60, move_types: [Type::FIRE], status: Status::BURN, status_probability: 10 }
+User.create([
+  { :name => 'Bot', :email => 'user@example.com', :password => 'secret' }
 ])
-
