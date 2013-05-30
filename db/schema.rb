9967f4f74877e62cf4f4704a86bc3d068c29114b
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-05-30 09:23:21 +0100

    Fixes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130530022118) do
+ActiveRecord::Schema.define(:version => 20130530070627) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -172,6 +172,10 @@ ActiveRecord::Schema.define(:version => 20130530022118) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.text     "about"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
